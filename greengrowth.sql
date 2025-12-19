02f209c520a54f9011e701cc88ad90d3565e6426
Hanif Musa Daut <velskynotfound@gmail.com>
2025-12-19 23:06:32 +0700

    Revisi maneh

--- greengrowth.sql
+++ greengrowth.sql
@@ -2,7 +2,8 @@
 SQLyog Community v12.4.0 (64 bit)
 MySQL - 8.0.30 : Database - greengrowth
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,7 +13,7 @@ MySQL - 8.0.30 : Database - greengrowth
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`greengrowth` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS`greengrowth`;
 
 USE `greengrowth`;
 
@@ -31,92 +32,9 @@ CREATE TABLE `admin_pemerintah` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`admin_id`),
   UNIQUE KEY `email_admin` (`email_admin`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 /*Data for the table `admin_pemerintah` */
-
-/*Table structure for table `artikel` */
-
-DROP TABLE IF EXISTS `artikel`;
-
-CREATE TABLE `artikel` (
-  `artikel_id` int NOT NULL AUTO_INCREMENT,
-  `judul_artikel` varchar(150) NOT NULL,
-  `deskripsi` text,
-  `foto_artikel` varchar(255) DEFAULT NULL,
-  `program_id` int NOT NULL,
-  `admin_id` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`artikel_id`),
-  KEY `fk_artikel_program` (`program_id`),
-  KEY `fk_artikel_admin` (`admin_id`),
-  CONSTRAINT `fk_artikel_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin_pemerintah` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_artikel_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-/*Data for the table `artikel` */
-
-/*Table structure for table `lamaran` */
-
-DROP TABLE IF EXISTS `lamaran`;
-
-CREATE TABLE `lamaran` (
-  `lamaran_id` int NOT NULL AUTO_INCREMENT,
-  `lowongan_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `status_lamaran` enum('menunggu','diterima','ditolak') DEFAULT 'menunggu',
-  `applied_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`lamaran_id`),
-  KEY `fk_lamaran_lowongan` (`lowongan_id`),
-  KEY `fk_lamaran_user` (`user_id`),
-  CONSTRAINT `fk_lamaran_lowongan` FOREIGN KEY (`lowongan_id`) REFERENCES `lowongan` (`lowongan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_lamaran_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-/*Data for the table `lamaran` */
-
-/*Table structure for table `laporan` */
-
-DROP TABLE IF EXISTS `laporan`;
-
-CREATE TABLE `laporan` (
-  `laporan_id` int NOT NULL AUTO_INCREMENT,
-  `program_id` int NOT NULL,
-  `foto_laporan` varchar(255) DEFAULT NULL,
-  `laporan_tanggal` date DEFAULT NULL,
-  `laporan_persentase_progres` decimal(5,2) DEFAULT NULL,
-  `laporan_output_ekonomi` bigint DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`laporan_id`),
-  KEY `fk_laporan_program` (`program_id`),
-  CONSTRAINT `fk_laporan_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-/*Data for the table `laporan` */
-
-/*Table structure for table `lowongan` */
-
-DROP TABLE IF EXISTS `lowongan`;
-
-CREATE TABLE `lowongan` (
-  `lowongan_id` int NOT NULL AUTO_INCREMENT,
-  `program_id` int NOT NULL,
-  `judul_lowongan` varchar(150) DEFAULT NULL,
-  `status_lowongan` enum('dibuka','ditutup') DEFAULT 'dibuka',
-  `lowongan_min_umur` int DEFAULT NULL,
-  `lowongan_max_umur` int DEFAULT NULL,
-  `lowongan_keahlian` text,
-  `lowongan_pengalaman` text,
-  `lowongan_min_pendidikan` varchar(100) DEFAULT NULL,
-  `kuota_pekerja` int DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`lowongan_id`),
-  KEY `fk_lowongan_program` (`program_id`),
-  CONSTRAINT `fk_lowongan_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-/*Data for the table `lowongan` */
 
 /*Table structure for table `program` */
 
@@ -135,27 +53,9 @@ CREATE TABLE `program` (
   PRIMARY KEY (`program_id`),
   KEY `fk_program_admin` (`admin_id`),
   CONSTRAINT `fk_program_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin_pemerintah` (`admin_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 /*Data for the table `program` */
-
-/*Table structure for table `statistik_program` */
-
-DROP TABLE IF EXISTS `statistik_program`;
-
-CREATE TABLE `statistik_program` (
-  `statistik_id` int NOT NULL AUTO_INCREMENT,
-  `program_id` int NOT NULL,
-  `laporan_id` int NOT NULL,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`statistik_id`),
-  UNIQUE KEY `program_id` (`program_id`),
-  KEY `fk_statistik_laporan` (`laporan_id`),
-  CONSTRAINT `fk_statistik_laporan` FOREIGN KEY (`laporan_id`) REFERENCES `laporan` (`laporan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_statistik_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-/*Data for the table `statistik_program` */
 
 /*Table structure for table `users` */
 
@@ -179,9 +79,110 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email_user` (`email_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 /*Data for the table `users` */
+
+/*Table structure for table `lowongan` */
+
+DROP TABLE IF EXISTS `lowongan`;
+
+CREATE TABLE `lowongan` (
+  `lowongan_id` int NOT NULL AUTO_INCREMENT,
+  `program_id` int NOT NULL,
+  `judul_lowongan` varchar(150) DEFAULT NULL,
+  `status_lowongan` enum('dibuka','ditutup') DEFAULT 'dibuka',
+  `lowongan_min_umur` int DEFAULT NULL,
+  `lowongan_max_umur` int DEFAULT NULL,
+  `lowongan_keahlian` text,
+  `lowongan_pengalaman` text,
+  `lowongan_min_pendidikan` varchar(100) DEFAULT NULL,
+  `kuota_pekerja` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`lowongan_id`),
+  KEY `fk_lowongan_program` (`program_id`),
+  CONSTRAINT `fk_lowongan_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/*Data for the table `lowongan` */
+
+/*Table structure for table `artikel` */
+
+DROP TABLE IF EXISTS `artikel`;
+
+CREATE TABLE `artikel` (
+  `artikel_id` int NOT NULL AUTO_INCREMENT,
+  `judul_artikel` varchar(150) NOT NULL,
+  `deskripsi` text,
+  `foto_artikel` varchar(255) DEFAULT NULL,
+  `program_id` int NOT NULL,
+  `admin_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`artikel_id`),
+  KEY `fk_artikel_program` (`program_id`),
+  KEY `fk_artikel_admin` (`admin_id`),
+  CONSTRAINT `fk_artikel_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin_pemerintah` (`admin_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_artikel_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/*Data for the table `artikel` */
+
+/*Table structure for table `lamaran` */
+
+DROP TABLE IF EXISTS `lamaran`;
+
+CREATE TABLE `lamaran` (
+  `lamaran_id` int NOT NULL AUTO_INCREMENT,
+  `lowongan_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `status_lamaran` enum('menunggu','diterima','ditolak') DEFAULT 'menunggu',
+  `applied_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`lamaran_id`),
+  KEY `fk_lamaran_lowongan` (`lowongan_id`),
+  KEY `fk_lamaran_user` (`user_id`),
+  CONSTRAINT `fk_lamaran_lowongan` FOREIGN KEY (`lowongan_id`) REFERENCES `lowongan` (`lowongan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_lamaran_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/*Data for the table `lamaran` */
+
+/*Table structure for table `laporan` */
+
+DROP TABLE IF EXISTS `laporan`;
+
+CREATE TABLE `laporan` (
+  `laporan_id` int NOT NULL AUTO_INCREMENT,
+  `program_id` int NOT NULL,
+  `foto_laporan` varchar(255) DEFAULT NULL,
+  `laporan_tanggal` date DEFAULT NULL,
+  `laporan_persentase_progres` decimal(5,2) DEFAULT NULL,
+  `laporan_output_ekonomi` bigint DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`laporan_id`),
+  KEY `fk_laporan_program` (`program_id`),
+  CONSTRAINT `fk_laporan_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/*Data for the table `laporan` */
+
+/*Table structure for table `statistik_program` */
+
+DROP TABLE IF EXISTS `statistik_program`;
+
+CREATE TABLE `statistik_program` (
+  `statistik_id` int NOT NULL AUTO_INCREMENT,
+  `program_id` int NOT NULL,
+  `laporan_id` int NOT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`statistik_id`),
+  UNIQUE KEY `program_id` (`program_id`),
+  KEY `fk_statistik_laporan` (`laporan_id`),
+  CONSTRAINT `fk_statistik_laporan` FOREIGN KEY (`laporan_id`) REFERENCES `laporan` (`laporan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_statistik_program` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/*Data for the table `statistik_program` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
