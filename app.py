b11d6c6e4fbb8223f07415a66ce057aaccae9279
Hanif Musa Daut <velskynotfound@gmail.com>
2025-12-20 23:23:32 +0700

    Try One

---
+++ app.py
@@ -0,0 +1,5 @@
+from greengrowth_project.app import app
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ greengrowth_project/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config['MYSQL_PORT'] = 3306
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "greengrowth"
+mysql = MySQL(app)
+
+
+@app.route('/')
+def index():
+    return "Asolale"
