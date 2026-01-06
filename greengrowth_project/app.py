7c78fb932c5148199cd93680f1ef9a6b5437cb24
sptra656 <muhammadfandy746@gmail.com>
2026-01-06 20:02:12 +0700

    project project

--- greengrowth_project/app.py
+++ greengrowth_project/app.py
@@ -19,11 +19,12 @@ from .controllers.user.lamaran import lamaran_user_bp
 load_dotenv()
 
 app = Flask(__name__)
-app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
-app.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT", 3306))
-app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
-app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
-app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MYSQL_HOST"] = "localhost"
+app.config['MYSQL_PORT'] = 3306
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "greengrowth"
 mysql = MySQL(app)
 
 # File upload config
