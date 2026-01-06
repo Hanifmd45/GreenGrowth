fc036df89e6550db9ff720a42c0f18ea06ea35e0
sptra656 <muhammadfandy746@gmail.com>
2026-01-06 18:50:54 +0700

    HOSTINGG

--- greengrowth_project/app.py
+++ greengrowth_project/app.py
@@ -20,10 +20,10 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
-app.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT", 3306))
+app.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT", 42925))
 app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
-app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+app.config["MYSQL_DATABASE"] = os.environ.get("MYSQL_DATABASE")
 mysql = MySQL(app)
 
 # File upload config
